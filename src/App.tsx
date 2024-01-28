8b882834750dc9dbcb6cd535c47b57433258f0e1
HA-SEUNG-JEONG <gktmd652@naver.com>
2024-01-28 19:13:04 +0900

    Chore : 초기 세팅 및 컴포넌트 제작 - tailwindcss 설치 - 초반 UI 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { IncludeLowercase } from "./components/IncludeLowercase";
+import IncludeNumbers from "./components/IncludeNumbers";
+import IncludeSpecialCharacter from "./components/IncludeSpecialCharacter";
+import IncludeUppercase from "./components/IncludeUppercase";
+import PasswordInput from "./components/PasswordInput";
+import PasswordLength from "./components/PasswordLength";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="rounded-lg border bg-card text-card-foreground shadow-sm">
+            <div className="flex flex-col space-y-1.5 p-6">
+                <div className="text-2xl font-bold">PassWord Generator</div>
+                <div className="text-sm text-muted-foreground">
+                    Generate a strong password using the options below
+                </div>
+            </div>
+            <div className="p-6 pt-0">
+                <div className="space-y-4">
+                    <div className="space-y-2">
+                        <PasswordInput />
+                        <PasswordLength />
+                    </div>
+                    <div className="space-y-2">
+                        <IncludeUppercase />
+                        <IncludeLowercase />
+                        <IncludeNumbers />
+                        <IncludeSpecialCharacter />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/PasswordLength.tsx
@@ -0,0 +1,21 @@
+const PasswordLength = () => {
+    return (
+        <>
+            <label
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                htmlFor="length"
+            >
+                Password Length
+            </label>
+            <input
+                className="flex h-10 w-full rounded-md border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 "
+                id="length"
+                placeholder="Enter password length"
+                required
+                type="number"
+            />
+        </>
+    );
+};
+
+export default PasswordLength;
