6d786983bdfb6c20188b0212b74e35c879bd66cb
HA-SEUNG-JEONG <gktmd652@naver.com>
2024-01-29 17:31:28 +0900

    Style : 슬라이더 개선 - 필요없는 스타일 제거 - onChange props로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -80,17 +80,16 @@ const App = () => {
                         <PasswordLength passwordLength={passwordLength} />
 
                         <input
-                            className="flex cursor-pointer h-10 rounded-md border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 "
+                            type="range"
+                            className="flex cursor-pointer"
                             id="length"
                             placeholder="Enter password length"
-                            required
-                            type="range"
                             min="1"
-                            max="20"
+                            max="30"
                             value={passwordLength}
-                            onInput={(event) => {
+                            onChange={(event) => {
                                 handlePasswordLengthChange(
-                                    Number(event.currentTarget.value)
+                                    Number(event.target.value)
                                 );
                             }}
                         />
