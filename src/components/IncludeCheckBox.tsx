0af4e8f8825c6fb8d3c5e520e91fe25bd0015e98
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-01-09 13:48:01 +0900

    fix : 코드 정리

--- src/components/IncludeCheckBox.tsx
+++ src/components/IncludeCheckBox.tsx
@@ -1,10 +1,17 @@
 import { onCheckboxChangeProps } from "../utils";
 
+interface IncludeCheckBoxProps extends onCheckboxChangeProps {
+    checked: boolean;
+}
+
 const IncludeCheckBox = ({
     onCheckboxChange,
     pattern,
     checked
-}: onCheckboxChangeProps & { checked: boolean }) => {
+}: IncludeCheckBoxProps) => {
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        onCheckboxChange(e.target.checked);
+    };
     return (
         <div className="flex items-center space-x-2">
             <input
@@ -12,7 +19,7 @@ const IncludeCheckBox = ({
                 className="peer h-4 w-4 shrink-0 rounded-sm border border-gray-900 ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-gray-900 data-[state=checked]:text-gray-50 "
                 id={pattern}
                 checked={checked}
-                onChange={(e) => onCheckboxChange(e.target.checked)}
+                onChange={onChange}
             />
             <label
                 className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
