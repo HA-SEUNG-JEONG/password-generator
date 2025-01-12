6f3647a3dc4d09ed7462e0294bcba9c252cae9c3
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-01-12 22:39:34 +0900

    fix :  X  버튼 삭제

--- src/components/PasswordInput.tsx
+++ src/components/PasswordInput.tsx
@@ -110,25 +110,6 @@ const PasswordInput = ({ value, onRefresh }: PasswordInputProps) => {
             </button>
             <div className="flex flex-col gap-2">
                 <div className="relative w-full">
-                    <button
-                        onClick={handleRemoveClipboard}
-                        className="flex items-center gap-2"
-                        aria-label="클립보드 지우기"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                        >
-                            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
-                        </svg>
-                    </button>
                     <input
                         className="flex h-10 w-full rounded-md border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:bg-dark-input dark:border-dark-border"
                         type={type}
