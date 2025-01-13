4b203f511b511fef7f4b0192dc58294b4303c6e9
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-01-13 14:36:22 +0900

    fix : 버튼 추가, 특정 알파벳,숫자,특수문자 반복 감지

--- src/components/PasswordDisplay.tsx
+++ src/components/PasswordDisplay.tsx
@@ -1,7 +1,8 @@
 // components/PasswordDisplay/index.tsx
 import { useState } from "react";
 import { toast } from "react-toastify";
-import { createCharacterSet, generatePassword } from "../utils/password";
+import EyeOn from "../assets/eye-1.svg";
+import EyeOff from "../assets/eye-off-1.svg";
 
 interface PasswordDisplayProps {
     password: string;
@@ -11,18 +12,23 @@ interface PasswordDisplayProps {
 const PasswordDisplay = ({ password, onRefresh }: PasswordDisplayProps) => {
     const [showPassword, setShowPassword] = useState(false);
 
-    const handleCopy = async () => {
-        if (!password) {
-            toast.error("비밀번호가 비어있습니다");
-            return;
-        }
-
+    const handlePasswordCopy = () => {
         try {
-            await navigator.clipboard.writeText(password);
-            toast.success("비밀번호가 복사되었습니다");
+            if (password.length === 0) toast.error("비밀번호가 비어있습니다.");
+            else {
+                navigator.clipboard.writeText(password);
+                toast.success("비밀번호가 복사되었습니다.");
+            }
         } catch (err) {
-            toast.error("복사에 실패했습니다");
+            if (err instanceof Error) toast.error(err.message);
         }
+    };
+
+    // 특정 문자/숫자 반복 감지
+    const hasRepeatingCharacters = (password: string) => {
+        // 특정 알파벳이나 특정 숫자 특정 특수문자가 2번 이상 반복되는지 확인
+        const regex = /(.)\1{1,}/g;
+        return regex.test(password);
     };
 
     return (
@@ -33,12 +39,67 @@ const PasswordDisplay = ({ password, onRefresh }: PasswordDisplayProps) => {
                 value={password}
                 readOnly
             />
+            {hasRepeatingCharacters(password) && (
+                <span className="text-xs text-red-500">
+                    반복되는 문자/숫자가 있습니다.
+                </span>
+            )}
             <div className="absolute right-2 top-1/2 -translate-y-1/2 flex space-x-2">
                 <button onClick={() => setShowPassword(!showPassword)}>
-                    {showPassword ? "숨기기" : "보기"}
+                    <img
+                        src={showPassword ? EyeOn : EyeOff}
+                        alt="Toggle Password"
+                    />
                 </button>
-                <button onClick={handleCopy}>복사</button>
-                <button onClick={onRefresh}>새로고침</button>
+                <button
+                    onClick={handlePasswordCopy}
+                    className="flex items-center gap-2"
+                    aria-label="복사하기"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    >
+                        <rect
+                            width="14"
+                            height="14"
+                            x="8"
+                            y="8"
+                            rx="2"
+                            ry="2"
+                        />
+                        <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
+                    </svg>
+                </button>
+                <button
+                    onClick={() => onRefresh()}
+                    className="flex items-center gap-2"
+                    aria-label="새로고침"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    >
+                        <path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8" />
+                        <path d="M21 3v5h-5" />
+                        <path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16" />
+                        <path d="M8 16H3v5" />
+                    </svg>
+                </button>
             </div>
         </div>
     );
