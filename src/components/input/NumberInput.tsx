2978b84e89cb28fc770617e4e9a5eb617ec93043
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-01-16 16:50:28 +0900

    fix : 속성 추가, label 추가

--- src/components/input/NumberInput.tsx
+++ src/components/input/NumberInput.tsx
@@ -5,9 +5,16 @@ interface NumberInputProps {
 const NumberInput = ({ handlePasswordChange }: NumberInputProps) => {
     return (
         <div className="flex items-center">
+            <label htmlFor="password-length" className="mr-2">
+                생성할 비밀번호 길이를 입력해주세요
+            </label>
             <input
                 type="number"
+                id="password-length"
                 placeholder="0"
+                min={8}
+                max={30}
+                value={length}
                 className="border border-black p-1 rounded-md"
                 onChange={handlePasswordChange}
             />
