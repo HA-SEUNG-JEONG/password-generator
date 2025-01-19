255c0035cd7d0559840cbd747ede87199ba9ea0b
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-01-19 10:52:01 +0900

    fix : 필요없는 UI 제거

--- src/components/PasswordLength.tsx
+++ src/components/PasswordLength.tsx
@@ -5,33 +5,12 @@ const PASSWORD_LENGTH = {
 } as const;
 
 const PasswordLength = ({ passwordLength }: { passwordLength: number }) => {
-    const getColorClass = () => {
-        if (passwordLength === 0) return "text-gray-400";
-        if (passwordLength < PASSWORD_LENGTH.MIN) return "text-red-600";
-        if (passwordLength < PASSWORD_LENGTH.RECOMMEND)
-            return "text-yellow-600";
-        return "text-green-600";
-    };
-
-    const getRecommendationMessage = (length: number) => {
-        if (length === 0) return "";
-        if (length < PASSWORD_LENGTH.MIN) return "(최소 8자 이상)";
-        if (length < PASSWORD_LENGTH.RECOMMEND) return "(추천: 12자 이상)";
-        return "(적정)";
-    };
-
     return (
-        <div
-            className={`flex items-center text-sm font-medium ${getColorClass()}`}
-        >
+        <div className={`flex items-center text-lg font-semibold `}>
             <div className="min-w-[7rem]">
                 {passwordLength === 0
                     ? "길이를 선택하세요"
                     : `비밀번호 길이: ${passwordLength}`}
-            </div>
-            <div className="text-sm font-bold">
-                {passwordLength > 0 &&
-                    `${getRecommendationMessage(passwordLength)}`}
             </div>
         </div>
     );
