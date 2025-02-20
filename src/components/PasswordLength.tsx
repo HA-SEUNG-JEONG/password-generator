e8b72d81098fe6f348da316e85b5fa7b1a932ffc
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-02-20 22:52:53 +0900

    delete : 주석 제거

--- src/components/PasswordLength.tsx
+++ src/components/PasswordLength.tsx
@@ -8,13 +8,6 @@ const PASSWORD_LENGTH = {
 
 const PasswordLength = ({ passwordLength }: { passwordLength: number }) => {
     return (
-        // <div className={`flex items-center text-lg font-semibold `}>
-        //     <div className="min-w-[7rem]">
-        //         {passwordLength === 0
-        //             ? "길이를 선택하세요"
-        //             : `비밀번호 길이: ${passwordLength}`}
-        //     </div>
-        // </div>
         <div
             className={css({
                 display: "flex",
