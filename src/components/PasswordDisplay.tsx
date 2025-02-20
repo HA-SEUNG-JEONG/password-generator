e27be1fc6b71e36a1e59c03c2135610a78604900
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-02-20 22:52:43 +0900

    refactor : svg 분리

--- src/components/PasswordDisplay.tsx
+++ src/components/PasswordDisplay.tsx
@@ -4,6 +4,8 @@ import EyeOn from "../assets/eye-1.svg";
 import EyeOff from "../assets/eye-off-1.svg";
 import PasswordStrengthIndicator from "./PasswordStrengthIndicator";
 import { css } from "../../styled-system/css";
+import CopyIcon from "./CopyIcon";
+import RefreshIcon from "./RefreshIcon";
 
 interface PasswordDisplayProps {
     password: string;
@@ -124,27 +126,7 @@ const PasswordDisplay = ({ password, onRefresh }: PasswordDisplayProps) => {
                     })}
                     aria-label="복사하기"
                 >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                    >
-                        <rect
-                            width="14"
-                            height="14"
-                            x="8"
-                            y="8"
-                            rx="2"
-                            ry="2"
-                        />
-                        <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
-                    </svg>
+                    <CopyIcon />
                 </button>
                 <button
                     onClick={() => onRefresh()}
@@ -155,22 +137,7 @@ const PasswordDisplay = ({ password, onRefresh }: PasswordDisplayProps) => {
                     })}
                     aria-label="새로고침"
                 >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                    >
-                        <path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8" />
-                        <path d="M21 3v5h-5" />
-                        <path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16" />
-                        <path d="M8 16H3v5" />
-                    </svg>
+                    <RefreshIcon />
                 </button>
             </div>
             <PasswordStrengthIndicator password={password} />
