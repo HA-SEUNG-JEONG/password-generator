282058ad4c7c781741ec11643c56d2bac10a03c5
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-02-21 19:53:28 +0900

    fix : App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { ThemeProvider } from "./context/ThemeContext";
 import PasswordGenerator from "./components/PasswordGenerator";
-import "../styled-system/styles.css";
+import { css } from "../styled-system/css";
 
 const App = () => {
     return (
         <ThemeProvider>
-            <div className="min-h-screen transition-colors duration-200  rounded-lg border bg-card text-card-foreground shadow-sm dark:bg-dark-bg dark:text-dark-text">
+            {/* <div className="min-h-screen transition-colors duration-200  rounded-lg border bg-card text-card-foreground shadow-sm dark:bg-dark-bg dark:text-dark-text">
+                <PasswordGenerator />
+            </div> */}
+            <div
+                className={css({
+                    minHeight: "screen",
+                    transition:
+                        "colors duration-200 rounded-lg border bg-card text-card-foreground shadow-sm dark:bg-dark-bg dark:text-dark-text"
+                })}
+            >
                 <PasswordGenerator />
             </div>
         </ThemeProvider>
