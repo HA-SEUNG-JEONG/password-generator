853a476aa4ff1635c193e63b27add7da2136ca70
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-02-21 20:04:24 +0900

    fix : 다크모드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,22 @@
-import { ThemeProvider } from "./context/ThemeContext";
 import PasswordGenerator from "./components/PasswordGenerator";
 import { css } from "../styled-system/css";
 
 const App = () => {
     return (
-        <ThemeProvider>
-            {/* <div className="min-h-screen transition-colors duration-200  rounded-lg border bg-card text-card-foreground shadow-sm dark:bg-dark-bg dark:text-dark-text">
-                <PasswordGenerator />
-            </div> */}
-            <div
-                className={css({
-                    minHeight: "screen",
-                    transitionProperty: "colors",
-                    transitionDuration: "200ms",
-                    borderRadius: "lg",
-                    border: "1px solid",
-                    bg: "card",
-                    color: "card-foreground",
-                    shadow: "sm",
-                    _dark: {
-                        bg: "dark-bg",
-                        color: "dark-text"
-                    }
-                })}
-            >
-                <PasswordGenerator />
-            </div>
-        </ThemeProvider>
+        <div
+            className={css({
+                minHeight: "screen",
+                transitionProperty: "colors",
+                transitionDuration: "200ms",
+                borderRadius: "lg",
+                border: "1px solid",
+                bg: "card",
+                color: "card-foreground",
+                shadow: "sm"
+            })}
+        >
+            <PasswordGenerator />
+        </div>
     );
 };
 
