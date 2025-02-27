655d2c3a475c0f9cbd3f6b378b9a8f2c5edc40c2
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-02-27 11:57:50 +0900

    fix : package.json 스크립트 수정

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import React from "react";
 import "../styled-system/styles.css";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
