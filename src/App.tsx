7939d158b48570a3d8b09d486293383470e8e1c1
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-02-27 11:41:49 +0900

    fix : 파일 원복

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { css } from "../styled-system/css";
 import PasswordGenerator from "./components/PasswordGenerator";
 
 const App = () => {
-    return <PasswordGenerator />;
+    return (
+        <div
+            className={css({
+                minHeight: "screen",
+                transitionProperty: "colors",
+                transitionDuration: "200ms",
+                borderRadius: "lg",
+                border: "1px solid",
+                bg: "card",
+                color: "card-foreground",
+                shadow: "sm"
+            })}
+        >
+            <PasswordGenerator />
+        </div>
+    );
 };
 
 export default App;
