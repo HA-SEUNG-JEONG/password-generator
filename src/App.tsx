28766e1ebc72b9d032e9741189547ae8ccac7ec6
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-02-27 10:53:55 +0900

    fix : App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
 import PasswordGenerator from "./components/PasswordGenerator";
-import { css } from "../styled-system/css";
 
 const App = () => {
-    return (
-        <div
-            className={css({
-                minHeight: "screen",
-                transitionProperty: "colors",
-                transitionDuration: "200ms",
-                borderRadius: "lg",
-                border: "1px solid",
-                bg: "card",
-                color: "card-foreground",
-                shadow: "sm"
-            })}
-        >
-            <PasswordGenerator />
-        </div>
-    );
+    return <PasswordGenerator />;
 };
 
 export default App;
